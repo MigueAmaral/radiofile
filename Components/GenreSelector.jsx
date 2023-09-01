ead522c944a6c1dfee5f912700433d15160fd07f
MigueAmaral <139987899+MigueAmaral@users.noreply.github.com>
2023-09-01 11:30:05 +0100

    Moooree fixes

--- Components/GenreSelector.jsx
+++ Components/GenreSelector.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Combobox } from "./ui/Combobox";
 
-export default function GenreSelector({genre, setGenre}) {
+export default function GenreSelector({ genre, setGenre }) {
   const [genreList, setGenreList] = useState();
 
   const servers = [
@@ -13,14 +13,23 @@ export default function GenreSelector({genre, setGenre}) {
 
   useEffect(() => {
     axios
-      .get("https://89.58.16.19/json/tags/?order=stationcount&reverse=true&limit=500")
+      .get(
+        "http://at1.api.radio-browser.info/json/tags/?order=stationcount&reverse=true&limit=500"
+      )
       .then((res) => setGenreList(res.data))
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <div className="border-solid border-2 border-slate-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 flex justify-center w-1/2 h-fit-content py-4 px-4 bg-slate-100 items-center mx-auto mt-4 rounded-md shadow-lg">
-      {genreList && <Combobox list={genreList} item={genre} setItem={setGenre} ticket={"genre"} />}
+      {genreList && (
+        <Combobox
+          list={genreList}
+          item={genre}
+          setItem={setGenre}
+          ticket={"genre"}
+        />
+      )}
     </div>
   );
 }
